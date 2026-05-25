ad1fff55cc5b017b88308f3b77f2693e6ae60b83
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-25 21:39:10 +0300

    more notes about multi lines and escape character

--- python/day01_hello.py
+++ python/day01_hello.py
@@ -2,3 +2,6 @@ print("Hello GitHub!") # this is a single line comment
 """ and this is a multi line comment
 which can span multiple lines and is often used for docstrings
 to describe functions, classes, or modules. """
+
+print("print function can print multiple lines by using escape characters like \\n for new line and \\t for tab.")
+print("This is the first line.\nThis is the second line.\tThis is after a tab.")
