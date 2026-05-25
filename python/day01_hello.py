9616b533c749f7de3c4724fc7bc3f8d2a82420d9
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-25 20:53:16 +0300

    this is how we can write a comments

--- python/day01_hello.py
+++ python/day01_hello.py
@@ -1,1 +1,4 @@
-print("Hello GitHub!")
+print("Hello GitHub!") # this is a single line comment
+""" and this is a multi line comment
+which can span multiple lines and is often used for docstrings
+to describe functions, classes, or modules. """
