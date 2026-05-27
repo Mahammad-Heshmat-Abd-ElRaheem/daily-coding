f1672c3c07748a75bf60cbda046d832be17cac43
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-27 23:36:20 +0300

    Keyword arguments

--- python/day02_operators.py
+++ python/day02_operators.py
@@ -4,3 +4,7 @@ print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
 print("My name is", "Python.")
 print("Monty Python.")
 
+#Keyword arguments
+print("My name is", name="Python.")
+print("Monty Python.", end=" ") # end parameter changes the default newline to a space
+print("I love programming in Python.")
