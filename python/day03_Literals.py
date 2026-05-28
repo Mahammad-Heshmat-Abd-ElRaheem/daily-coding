bf394831a10cab0719e2f032a2504d90b23dee5f
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-28 17:00:21 +0300

    how to print a different bases numbers

--- python/day03_Literals.py
+++ python/day03_Literals.py
@@ -9,3 +9,9 @@ print(+500) # prints the positive integer +500
 # if the number is long we can add _ to improve readability
 print(1_000_000) # prints the integer 1000000
 print(3.141_592_653_589_793) # prints the float 3.141592653589793
+
+# we can also print numbers in different bases using prefixes
+print(0b1010) # prints the binary number 10 (base 2)
+print(0o17) # prints the octal number 15 (base 8)
+print(0x1F) # prints the hexadecimal number 31 (base 16)
+
