53cc2827380476dc9135ed1991e7ea02bb168b09
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-28 18:34:32 +0300

    different types of using escape characters

---
+++ python/day04_strings.py
@@ -0,0 +1,15 @@
+#  escape character, which you should remember is played by the backslash
+print("I like \"Monty Python\"")
+
+# Python can use an apostrophe instead of a quote
+print('I like "Monty Python"')
+#  but if you want to use an apostrophe, you have to escape it
+print('I\'m Monty Python.') # or
+print("I'm Monty Python.")
+#  you can also use a raw string, which ignores escape characters
+print(r"I'm Monty Python.") 
+#  triple quotes can also be used for multi-line strings
+print("""This is a multi-line string.
+It spans multiple lines.""")    
+#  you can also use triple quotes for single-line strings
+print("""This is a single-line string.""")
