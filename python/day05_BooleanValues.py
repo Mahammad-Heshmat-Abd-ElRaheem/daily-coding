27efba1942cd1f2b180f9e2b0c61885b2fecea98
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-28 18:55:52 +0300

    modify some elements

--- python/day05_BooleanValues.py
+++ python/day05_BooleanValues.py
@@ -1,15 +1,15 @@
-#These two Boolean values have strict denotations in Python:
+# These two Boolean values have strict denotations in Python:
 print(True)  # prints the Boolean value True
 print(False)  # prints the Boolean value False
 # you have to take these symbols as they are, including case-sensitivity.
-#In Python, the following values are considered False:
+# In Python, the following values are considered False:
 # - None
 # - False
 # - 0
 # - "" (empty string)
 # - [] (empty list)
 # - {} (empty dictionary)
-#All other values are considered True. For example:
+# All other values are considered True. For example:
 print(bool(0))  # prints False
 print(bool(1))  # prints True
 print(bool("Hello"))  # prints True
