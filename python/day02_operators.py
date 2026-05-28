1ff8599e334abe3215976af8b44556e80198660a
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-28 14:42:44 +0300

    more features aboat multible arrguments

--- python/day02_operators.py
+++ python/day02_operators.py
@@ -1,3 +1,4 @@
+#Using multiple arguments
 print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
 
 #Positional arguments
@@ -5,6 +6,12 @@ print("My name is", "Python.")
 print("Monty Python.")
 
 #Keyword arguments
-print("My name is", name="Python.")
 print("Monty Python.", end=" ") # end parameter changes the default newline to a space
 print("I love programming in Python.")
+
+# usully print() function separates its outputted arguments with spaces. This behavior can be changed
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.", sep="-") # sep parameter changes the default space separator to a hyphen
+
+#Both keyword arguments may be mixed in one invocation, just like here in the editor window.
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
