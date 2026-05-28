e154fa6af16a87c1d810eb483d2291b0c6e74c0e
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-28 16:36:05 +0300

    how to print literals

---
+++ python/day03_Literals.py
@@ -0,0 +1,11 @@
+# we can print numbers (integers and floats) directly without converting them to strings
+print(1000) # prints the integer 1000
+print(3.14) # prints the float 3.14
+
+# we can print negative value as well
+print(-500) # prints the negative integer -500
+print(+500) # prints the positive integer +500
+
+# if the number is long we can add _ to improve readability
+print(1_000_000) # prints the integer 1000000
+print(3.141_592_653_589_793) # prints the float 3.141592653589793
