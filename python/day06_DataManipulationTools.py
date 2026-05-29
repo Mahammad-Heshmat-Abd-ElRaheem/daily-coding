6a91d25e9fad6957790518b9c544c61c00321e62
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-29 15:23:51 +0300

    floor division

--- python/day06_DataManipulationTools.py
+++ python/day06_DataManipulationTools.py
@@ -33,3 +33,16 @@ print(10 / 3)   # the result is 3.3333333333333335
 print(10 / 3.)  # the result is 3.3333333333333335
 print(10. / 3)  # the result is 3.3333333333333335
 print(10. / 3.) # the result is 3.3333333333333335
+
+# Integer division (floor division): the result is the largest integer less than or equal to the division result.
+print(10 // 3)   # the result is 3
+print(10 // 3.)  # the result is 3.0
+print(10. // 3)  # the result is 3.0
+print(10. // 3.) # the result is 3.0
+# the results are always rounded; the result is an integer if both arguments are integers, and a float if at least one argument is a float.
+# note: This is very important: rounding always goes to the lesser integer
+print(-6 // 4)  # the result is -2, not -1, because -2 is less than -1
+print(6. // -4) # the result is -2.0, not -1.0, because -2.0 is less than -1.0
+
+
+
