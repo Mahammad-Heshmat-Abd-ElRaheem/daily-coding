d7740aa9e28c6f08d19107fb927fa3c66a6b9a9f
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-29 12:51:00 +0300

    data manipulation tools

---
+++ python/day06_DataManipulationTools.py
@@ -0,0 +1,15 @@
+# Python as a calculator
+print(2 + 3)  # prints 5
+print(10 - 4)  # prints 6
+print(7 * 8)  # prints 56
+print(15 / 3)  # prints 5.0
+
+# Basic operators which are used for data manipulation:
+# - Addition (+)
+# - Subtraction (-)
+# - Multiplication (*)
+# - Division (/)
+# - Modulo (%)
+# - Exponentiation (**)
+# - Floor Division (//)
+
