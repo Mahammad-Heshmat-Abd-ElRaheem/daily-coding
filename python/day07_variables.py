5c1dfa4a8e2d671b3e7c297374d6850414285792
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-30 08:40:43 +0300

    Variable Exeplaination

---
+++ python/day07_variables.py
@@ -0,0 +1,9 @@
+############################## Variable names ##############################
+# Variable names can only contain letters, numbers, and underscores.
+# Variable names cannot start with a number.
+# Variable names cannot be the same as Python keywords (reserved words).
+# Variable names should be descriptive and meaningful.
+# Examples of valid variable names:
+my_variable = 10
+their_variable = 20
+our_variable = 30
