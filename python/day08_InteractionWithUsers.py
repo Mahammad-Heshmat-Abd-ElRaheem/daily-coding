17a10efecf71142a9f4f370cdf8940750cb3c92b
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-05-30 16:38:55 +0300

    Explaining the input function

---
+++ python/day08_InteractionWithUsers.py
@@ -0,0 +1,7 @@
+#################################### How to interact with users in Python ####################################
+# the answer is: The input() function
+# The input() function allows you to take input from the user. It prompts the user to enter some data and returns it as a string.
+# Example of using the input() function:
+print("What is your name?")
+name = input()  # This will wait for the user to enter their name and store it in the variable 'name'
+print("Hello", name)  # This will greet the user by their name
