3a35786cce3a3881a81de7ddb00d19630e7351ff
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-01 09:20:39 +0300

    explaination of more comparison operation

--- python/day09_MakingDecision.py
+++ python/day09_MakingDecision.py
@@ -34,3 +34,16 @@ print(2 < 1) # False
 print(1 < 1) # False
 print("a" < "b") # True
 print("b" < "a") # False
+
+################################### Comparison: greater than or equal to operator ###################################
+#  the greater than or equal to operator is >=, and it returns True if the left operand is greater than or equal to the right operand, and False otherwise
+print(1 >= 2) # False
+print(2 >= 1) # True
+print(1 >= 1) # True
+
+################################### Comparison: less than or equal to operator ###################################
+#  the less than or equal to operator is <=, and it returns True if the left operand is less than or equal to the right operand, and False otherwise
+print(1 <= 2) # True   
+print(2 <= 1) # False
+print(1 <= 1) # True
+
