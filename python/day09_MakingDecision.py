a25de8bbf23800c27a0c85b77aeae89c594d4301
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-01 09:31:17 +0300

    update the priority table

--- python/day09_MakingDecision.py
+++ python/day09_MakingDecision.py
@@ -47,3 +47,25 @@ print(1 <= 2) # True
 print(2 <= 1) # False
 print(1 <= 1) # True
 
+################################### Making use of comparison operators ###################################
+#  we can use comparison operators to make decisions in our code. For example, we can memorize the result of a comparison in a variable, and then use that variable to make a decision
+
+number_of_students = 35
+answer = number_of_students > 30
+print(answer) # True
+
+################################### Priority of comparison operators ###################################
+#  comparison operators have a specific priority order:
+# 1 - parentheses ()
+# 2 - unary operators +, -
+# 3 - the power operator **
+# 4 - multiplication *, division /, floor division //, modulus %
+# 5 - addition +, subtraction -
+# 6 - comparison operators >, <, >=, <=
+# 7 - equality operators ==, !=
+
+# The operators == and != have lower priority than the comparison operators >, <, >=, <=
+#  this means that in an expression like 1 == 2 > 3, the > operator is evaluated first, and then the == operator
+print(1 == 2 > 3) # False
+print(1 == 2 < 3) # False because 2 < 3 is True, but 1 == True is False
+print(1 == 1 > 0) # True because 1 > 0 is True, and 1 == True is True
