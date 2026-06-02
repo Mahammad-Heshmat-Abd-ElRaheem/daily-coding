d2f985da1962f4895cccb38673dd497447a0b2e6
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-02 08:08:41 +0300

    Explaining IF statment

---
+++ python/day11_If_Condition.py
@@ -0,0 +1,21 @@
+########################################################################################
+# If Condition
+########################################################################################
+# If condition is used to execute a block of code only if a specified condition is true.
+# The syntax of an if statement is as follows:
+# if condition:
+#     block of code
+# The condition is a boolean expression that evaluates to either True or False. If the condition is True, the block of code is executed. If the condition is False, the block of code is skipped.
+# Example of an if statement
+""" 
+if the_weather_is_good:
+    print("Let's go for a walk.")
+
+if sheep_counter >= 50 :
+    print("There are enough sheep to make a wool blanket.")
+"""
+
+age = 18
+if age >= 18:
+    print("You are eligible to vote.")
+
