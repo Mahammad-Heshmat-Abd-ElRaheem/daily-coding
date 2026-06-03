d73af87e93a4e637c5ba0abe2f1ccf42324612c0
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-03 22:48:14 +0300

    print methods

--- python/day04_strings.py
+++ python/day04_strings.py
@@ -21,3 +21,51 @@ print("""This is a single-line string.""")
 print("Hello, " + "World!") #the output will be: Hello, World!
 #  you can also repeat strings using the * operator
 print("Hello, " * 3) # the output will be: Hello, Hello, Hello,
+
+################################ String indexing and slicing ##############################
+#  you can access individual characters in a string using indexing
+print("Hello, World!"[0]) # the output will be: H
+print("Hello, World!"[1:5]) # the output will be: ello  
+print("Hello, World!"[-6:-1]) # the output will be: World
+
+################################ String methods ######################################
+#  strings have many built-in methods that can be used to manipulate them
+print("hello, world!".upper()) # the output will be: HELLO, WORLD!
+print("HELLO, WORLD!".lower()) # the output will be: hello, world!
+print("   hello, world!   ".strip()) # the output will be: hello, world!
+print("hello, world!".replace("o", "0")) # the output will be: hell0, w0rld!
+print("hello, world!".split(", ")) # the output will be: ['hello', 'world!']
+print("hello, world!".find("world")) # the output will be: 7
+print("hello, world!".startswith("hello")) # the output will be: True
+print("hello, world!".endswith("!")) # the output will be: True
+print("hello, world!".count("o")) # the output will be: 2
+print("hello, world!".isalpha()) # the output will be: False (because of the comma and space)
+print("hello".isalpha()) # the output will be: True
+print("123".isdigit()) # the output will be: True
+print("hello, world!".title()) # the output will be: Hello, World!
+print("hello, world!".capitalize()) # the output will be: Hello, world!
+print("hello, world!".center(20, "*")) # the output will be: ***hello, world!*******
+print("hello, world!".ljust(20, "*")) # the output will be: hello, world!********
+print("hello, world!".rjust(20, "*")) # the output will be: *******hello, world!
+print("hello, world!".zfill(20)) # the output will be: 0000000000hello, world!
+print("hello, world!".lstrip()) # the output will be: hello, world! (removes leading whitespace)
+print("hello, world!".rstrip()) # the output will be: hello, world! (removes trailing whitespace)
+print("hello, world!".partition(", ")) # the output will be: ('hello', ', ', 'world!')
+print("hello, world!".rpartition(", ")) # the output will be: ('hello', ', ', 'world!')
+print("hello, world!".splitlines()) # the output will be: ['hello, world!'] (splits on newlines)
+print("hello, world!\nThis is a new line.".splitlines()) # the output will be: ['hello, world!', 'This is a new line.']
+print("hello, world!".swapcase()) # the output will be: HELLO, WORLD! (swaps case of each character)
+print("hello, world!".isupper()) # the output will be: False
+print("HELLO, WORLD!".isupper()) # the output will be: True
+print("hello, world!".islower()) # the output will be: True
+print("HELLO, WORLD!".islower()) # the output will be: False
+print("hello, world!".isprintable()) # the output will be: True
+print("hello, world!\n".isprintable()) # the output will be: False
+print("hello, world!".isidentifier()) # the output will be: False (because of the comma and space)
+print("hello_world".isidentifier()) # the output will be: True
+print("hello, world!".isdecimal()) # the output will be: False
+print("123".isdecimal()) # the output will be: True
+print("hello, world!".isnumeric()) # the output will be: False
+print("123".isnumeric()) # the output will be: True
+print("hello, world!".isascii()) # the output will be: True
+print("hello, world! 👋".isascii()) # the output will be: False
