f01d799c9849456b798079964ccac8024efefbe3
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-07 23:36:03 +0300

    how to generate a random number

--- python/Problem_Solving/0_Introduction/5_GenerateRandomNumbers.py
+++ python/Problem_Solving/0_Introduction/5_GenerateRandomNumbers.py
@@ -1,2 +1,10 @@
-num = float(input("Enter a number: "))
-print("The square root of", num, "is", num ** 0.5)
+# Python can generate random numbers using the built-in `random` module. Here's how you can generate a random number between 1 and 100:
+
+# Generate a random number between 1 and 100
+from random import random
+
+
+random_number = random.randint(1, 100)
+print("Random number:", random_number)
+
+# You can also generate a random floating-point number between 0 and 1 using the `random()` function:
