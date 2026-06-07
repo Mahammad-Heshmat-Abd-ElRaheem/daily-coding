a438a5dc34599690e28bedee883096b92d73381c
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-07 23:39:51 +0300

    swapping two var without temp variable

--- python/Problem_Solving/0_Introduction/4_SwapTwoVariables.py
+++ python/Problem_Solving/0_Introduction/4_SwapTwoVariables.py
@@ -7,3 +7,12 @@ num2 = temp
 print("After swapping:")
 print("First number:", num1)
 print("Second number:", num2)
+
+# Alternatively, you can swap two variables in Python without using a temporary variable:
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Swapping the values without a temporary variable
+num1, num2 = num2, num1
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
