ea1be5847ed4bfb811750c0652e5fd9ed527bbaf
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-07 23:38:21 +0300

    more features about printing random numbers using random library

--- python/Problem_Solving/0_Introduction/5_GenerateRandomNumbers.py
+++ python/Problem_Solving/0_Introduction/5_GenerateRandomNumbers.py
@@ -8,3 +8,8 @@ random_number = random.randint(1, 100)
 print("Random number:", random_number)
 
 # You can also generate a random floating-point number between 0 and 1 using the `random()` function:
+random_float = random.random()
+print("Random float between 0 and 1:", random_float)
+# If you want to generate a random number within a specific range, you can use the `randint()` function:
+random_number_in_range = random.randint(10, 50)
+print("Random number in range 10-50:", random_number_in_range)
