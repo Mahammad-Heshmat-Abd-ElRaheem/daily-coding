c38fbf704b5320207111f5960eddb6351a3162c3
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-07 23:33:32 +0300

    how to swap two numbers

--- python/Problem_Solving/0_Introduction/4_SwapTwoVariables.py
+++ python/Problem_Solving/0_Introduction/4_SwapTwoVariables.py
@@ -1,2 +1,9 @@
-num = float(input("Enter a number: "))
-print("The square root of", num, "is", num ** 0.5)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# Swapping the values
+temp = num1 
+num1 = num2
+num2 = temp
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
