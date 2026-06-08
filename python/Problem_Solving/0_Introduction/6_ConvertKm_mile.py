ab9f50d2d78940f63cae32c2f8b617ead478cb86
Muhammad_Heshmat <mahammad.heshmat.abdel.raheem@gmail.com>
2026-06-08 22:10:53 +0300

    added one more excersies on python introduction dir

--- python/Problem_Solving/0_Introduction/6_ConvertKm_mile.py
+++ python/Problem_Solving/0_Introduction/6_ConvertKm_mile.py
@@ -1,3 +1,3 @@
 km = float(input("please enter a value of km: "))
-mile= (.7 * km)
+mile= (0.621371 * km)
 print(f"{km} km equal -> {mile} mile")
---
+++ python/Problem_Solving/0_Introduction/7_ConvertCelsiusToFahrenheit.py
@@ -0,0 +1,3 @@
+Celsius = float(input("Enter the degree by C: "))
+Fahrenheit = (Celsius * 1.8) + 32
+print(f"{Celsius} C equal -> {Fahrenheit} F")
